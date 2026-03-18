840efb41ecc0ad4713ee06ef1c2f1adb347429d4
Anna Buckel <abuckel0503@gmail.com>
2026-03-18 14:41:14 +0000

    /years complete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
-from sqlalchemy import select
-from sqlmodel import Session
+from sqlmodel import Session, select
 from models import engine, People, Teams, Batting
 
 app = FastAPI()
@@ -11,7 +10,7 @@ async def get_years():
     with Session(engine) as session:
         statement = select(Teams.yearid).distinct().order_by(Teams.yearid)
         years = session.exec(statement).all()
-        return years
+    return years
 
 
 app.mount("/", StaticFiles(directory="static", html=True),name="static")
